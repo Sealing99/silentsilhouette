8a80d4911d1ef7ec23cfeb2896be8b9d722f9833
Sealing99 <bramelientje1@gmail.com>
2026-05-08 16:02:16 +0200

    tried to fix crucified

--- src/main/java/net/sealing99/silentsilhouette/entity/custom/SilhouetteEntity.java
+++ src/main/java/net/sealing99/silentsilhouette/entity/custom/SilhouetteEntity.java
@@ -6,17 +6,24 @@ import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import net.sealing99.silentsilhouette.TheSilentSilhouette;
 import org.jetbrains.annotations.Nullable;
 
-public class SilhouetteEntity extends AnimalEntity {
+public class SilhouetteEntity extends PathAwareEntity {
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
+
+    private boolean isCrucified = false;
+    private int crucificationTimeout = 0;
 
     public SilhouetteEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
@@ -33,7 +40,7 @@ public class SilhouetteEntity extends AnimalEntity {
 
     public static DefaultAttributeContainer.Builder createAttributes() {
         return MobEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 200)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 100)
                 .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 1)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 14)
@@ -50,21 +57,37 @@ public class SilhouetteEntity extends AnimalEntity {
     }
 
     @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        if (!this.getWorld().isClient()) {
+            if (this.crucificationTimeout == 0) {
+                this.isCrucified = !this.isCrucified;
+                TheSilentSilhouette.LOGGER.info("Silhouette crucified state toggled: " + this.isCrucified);
+                this.crucificationTimeout = 10;
+                TheSilentSilhouette.LOGGER.info("Silhouette crucification timeout set to: " + this.crucificationTimeout);
+            }
+
+            return ActionResult.SUCCESS;
+        }
+        return ActionResult.PASS;
+    }
+
+
+    @Override
     public void tick() {
         super.tick();
 
         if (this.getWorld().isClient()) {
             this.setupAnimationStates();
+
+            if (this.age % 2 == 0 && this.getHealth() < this.getMaxHealth() && !this.isCrucified) {
+                this.setHealth(100.0f);
+                TheSilentSilhouette.LOGGER.info("Silhouette healed to full health!");
+            }
+
+            if (this.crucificationTimeout > 0) {
+                this.crucificationTimeout = this.crucificationTimeout - 1;
+            }
+            TheSilentSilhouette.LOGGER.info("Silhouette crucification timeout: " + this.crucificationTimeout);
         }
-    }
-
-    @Override
-    public boolean isBreedingItem(ItemStack stack) {
-        return false;
-    }
-
-    @Override
-    public @Nullable PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
-        return null;
     }
 }
